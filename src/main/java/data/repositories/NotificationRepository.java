0bc5e54f55995822337ebe0a93c0dfd6c3ead92f
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-03-22 10:35:26 -0500

    notification repository implemented

---
+++ src/main/java/data/repositories/NotificationRepository.java
@@ -0,0 +1,7 @@
+package data.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import data.entities.Notification;
+
+public interface NotificationRepository extends MongoRepository<Notification, Long> {
+}
