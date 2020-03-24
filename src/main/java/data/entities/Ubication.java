e06d095e2f3a7b658476e5170e5b34ded2cbcb40
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-03-23 20:07:10 -0500

    user attribute of ubication exclude from json

--- src/main/java/data/entities/Ubication.java
+++ src/main/java/data/entities/Ubication.java
@@ -3,6 +3,7 @@ package data.entities;
 import java.io.Serializable;
 import java.time.ZonedDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -26,6 +27,7 @@ public class Ubication implements Serializable {
     private Double longitude;
 
     @DBRef
+    @JsonIgnore
     private UserApp user;
 
     public Ubication() {}
