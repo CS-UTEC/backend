4391e2e774a4f0019530295aedeb41ccdde9c827
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-03-24 22:58:10 -0500

    route created for testing

--- src/main/java/controller/UbicationController.java
+++ src/main/java/controller/UbicationController.java
@@ -5,6 +5,8 @@ import services.UserAppService;
 import data.entities.*;
 import data.models.*;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class UbicationController {
 
     @Autowired
-    private UbicationService ubicationService;
+    private UbicationService service;
 
     @Autowired
     private UserAppService appService;
 
-    
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<?> find() {
-        return new ResponseEntity<>(ubicationService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
-    
 
     @RequestMapping(value = "/{id}", method = RequestMethod.POST)
     public ResponseEntity<?> create(@PathVariable String id, @RequestBody UbicationModel input) {
@@ -34,7 +34,19 @@ public class UbicationController {
         if (user == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        ubicationService.create(input, user);
+        service.create(input, user);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/circle", method = RequestMethod.POST)
+    public ResponseEntity<?> findByCircleArea(@RequestBody CircleArea input) {
+        List<Ubication> circleArea = service.findByCircle(input.getCenter(), input.getRadius());
+        return new ResponseEntity<>(circleArea, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/box", method = RequestMethod.POST)
+    public ResponseEntity<?> findByBoxArea(@RequestBody BoxArea input) {
+        List<Ubication> boxArea = service.findByBox(input.getStart(), input.getFinal());
+        return new ResponseEntity<>(boxArea, HttpStatus.OK);
     }
 }
