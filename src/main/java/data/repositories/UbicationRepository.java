6880e8f5c25b7b1f8bbe8d70d58541140d5d3cd9
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-03-24 22:55:51 -0500

    querys for search by box and circle area

--- src/main/java/data/repositories/UbicationRepository.java
+++ src/main/java/data/repositories/UbicationRepository.java
@@ -1,7 +1,13 @@
 package data.repositories;
 
+import java.util.List;
+
+import org.springframework.data.geo.Box;
+import org.springframework.data.geo.Circle;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import data.entities.Ubication;
 
 public interface UbicationRepository extends MongoRepository<Ubication, String> {
+    List<Ubication> findByLocationWithin(Circle circle);
+    List<Ubication> findByLocationWithin(Box box);
 }
